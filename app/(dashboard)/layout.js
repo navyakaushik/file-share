0526bbec665ba685efcd4eeda40a6f0a32f5f42c
navyakaushik <navyakaushik678@gmail.com>
2024-04-28 14:58:33 +0530

    Initial commit

---
+++ app/(dashboard)/layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function layout({children}) {
+  return (
+    <div>
+        Home
+        {children}
+    </div>
+  )
+}
+
+export default layout
---
+++ app/_components/_utils/Constant.js
@@ -0,0 +1,3 @@
+export default{
+    desc: 'Drag and drop your files onto our secure cloud platform and share files securely with friends directly via email. Experience effortless file sharing, simplified.'
+}
